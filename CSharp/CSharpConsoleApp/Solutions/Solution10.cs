de2e7d6580f32af903885e7b21c75609519a67f6
unknown <caoyingzhe@gmail.com>
2021-04-19 08:51:18 +0900

    Personal access tokens Test

---
+++ CSharp/CSharpConsoleApp/Solutions/Solution10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CSharpConsoleApp.Solutions
+{
+    class Solution10 : SolutionBase
+    {
+        /// <summary>
+        /// 难易度:
+        /// </summary>
+        public override Difficulity GetDifficulity() { return Difficulity.Hard; }
+        /// <summary>
+        /// 关键字:
+        /// </summary>
+        public override string[] GetKeyWords() { return new string[] { "" }; }
+        /// <summary>
+        /// 标签：
+        /// </summary>
+        public override Tag[] GetTags() { return new Tag[] { Tag.String, Tag.DynamicProgramming, Tag.Backtracking }; }
+
+        public override bool Test(Stopwatch sw)
+        {
+            bool isSuccess = true;
+            return isSuccess;
+        }
+    }
+}
